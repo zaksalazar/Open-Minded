66ca28057586d56a4bf80f936a08a945907fd17b
zaksalazar <zsalazar.tv@gmail.com>
2023-01-17 18:18:37 -0800

    updated repo code

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,4 @@
 DROP DATABASE IF EXISTS open_minded;
 
 CREATE DATABASE open_minded; 
-USE open_minded;
 
-CREATE TABLE questions (
-  id INT NOT NULL AUTO_INCREMENT,
-  question TEXT NOT NULL,
-  category TEXT NOT NULL
-);
