adfc524fe6b6d9b0152b1dbbdb76ad1df9920683
Kaz Nyborg-Andersen <113869318+kaznyborg@users.noreply.github.com>
2023-01-26 18:59:19 -0800

    timer_start_and_CS

--- public/main.js
+++ public/main.js
@@ -131,26 +131,3 @@ document
   .addEventListener('click', leaveAndRemoveLocalStream);
 document.getElementById('mic-btn').addEventListener('click', toggleMic);
 document.getElementById('camera-btn').addEventListener('click', toggleCamera);
-
-  // const startingMinutes = 3;
-  // let time = startingMinutes * 60;
-  
-  // const countdownEl = document.getElementById("countdown");
-  
-  // setInterval(updateCountdown, 1000);
-  
-  // function updateCountdown() {
-  //   const minutes = Math.floor(time / 60);
-  //   let seconds = time % 60;
-  
-  //   seconds = seconds < 10 ? "0" + seconds : seconds
-  
-  //   countdownEl.innerHTML = (`${minutes} ${seconds}`);
-  //   time--
-  // }
-  
-  // setInterval("location.reload(true)", 300000);
-  
-  // setTimeout(function(){
-  //   window.location.reload();
-  // }, 180000);
